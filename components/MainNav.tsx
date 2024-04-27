bbba281732a6b9d85e5f29eecaa5af20fe7fa7f1
salmanpatrick5@gmail.com <salmanpatrick5@gmail.com>
2024-04-27 17:44:40 +0500

    updated nav

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -5,12 +5,16 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
 
 const MainNav: React.FC = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-8 bg-neutral-900 text-white shadow-md">
-      <div className="text-lg font-bold drop-shadow-md">Auctions Hub</div>
-      <div>
+      <div className="text-lg font-bold drop-shadow-md">
+        <Link href={`${process.env.NEXT_PUBLIC_URL}`}>Auctions Hub</Link>
+      </div>
+      <div className="flex items-center gap-5">
+        <Link href={`${process.env.NEXT_PUBLIC_URL}/dashboard`}>Dashboard</Link>
         <SignedIn>
           <UserButton />
         </SignedIn>
